b212f8b4ed380b74a6acf537c813ef7d6a7f3d4f
junilhwang <junil.h@kakao.com>
2025-07-09 11:05:04 +0900

    fix: 상수 관련 설정 수정

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
 import { defineConfig } from "vite";
 import { resolve } from "path";
 
+const base = process.env.NODE_ENV === "production" ? "/front_6th_chapter1-1/" : "";
+
 export default mergeConfig(
   defineConfig({
-    base: "/hanghae-plus/front_6th_chapter1-1/",
+    base,
     build: {
       rollupOptions: {
         input: {
